e7d3361a16fbba197497caeb75af5e76b53cd296
Lê Anh Tuấn <leanhtuan1996@users.noreply.github.com>
2017-11-15 15:47:19 +0700

    INIT PROJECT

---
+++ DebtBook/Classes/UIs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DebtBook
+//
+//  Created by Lê Anh Tuấn on 11/15/17.
+//  Copyright © 2017 Lê Anh Tuấn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
